a9d02cadf588aad339e76911c015f835c2d6640b
chongruei <chongruei@gmail.com>
2022-07-09 21:59:01 +0800

    feat: add react query lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "normalize.css";
 import "@styles/globals.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
