419f2b2cf8f4e3ecfd0c40e4cf0a7fcd3dad68a7
chongruei <chongruei@gmail.com>
2022-07-10 14:28:34 +0800

    test: fix test cases to pass the scenario

--- cypress/e2e/pages/index.cy.ts
+++ cypress/e2e/pages/index.cy.ts
@@ -1,12 +1,16 @@
 describe("Index Page", () => {
-  it("should render exchanes correctly", () => {
+  it("should visit index correctly", () => {
     // Start from the index page
     cy.visit("http://localhost:3000/");
+  });
 
+  it("should render exchanges header correctly", () => {
     cy.getByTestId("exchange-rank").contains("Rank");
     cy.getByTestId("exchange-name").contains("Name");
     cy.getByTestId("exchange-country").contains("Country");
+  });
 
+  it("should render exchanges columns correctly", () => {
     // next page
     cy.getByTestId("next-page").click();
 
@@ -20,16 +24,16 @@ describe("Index Page", () => {
     cy.getByTestId("rank-13").contains("13");
 
     // prev page
-    cy.getByTestId("next-page").click();
+    cy.getByTestId("prev-page").click();
 
     // rank 11
-    cy.getByTestId("rank-1").contains("11");
+    cy.getByTestId("rank-1").contains("1");
 
     // rank 12
-    cy.getByTestId("rank-2").contains("12");
+    cy.getByTestId("rank-2").contains("2");
 
     // rank 13
-    cy.getByTestId("rank-3").contains("13");
+    cy.getByTestId("rank-3").contains("3");
   });
 });
 
