1c26d69d32753b3946b023b3a65b1f2ee62acddf
chongruei <chongruei@gmail.com>
2022-07-10 17:31:47 +0800

    feat(interface): add exchange detail entity interface

--- interface/exchange.ts
+++ interface/exchange.ts
@@ -12,3 +12,17 @@ export type ExchangeEntity = {
   trade_volume_24h_btc: number;
   trade_volume_24h_btc_normalized: number;
 };
+
+export type ExchangeDetailEntity = {
+  name: string;
+  year_established: number;
+  country: string;
+  description: string;
+  url: string;
+  image: string;
+  trust_score_rank: number;
+  facebook_url: string;
+  reddit_url: string;
+  telegram_url: string;
+  slack_url: string;
+};
